0ed299d20645ee3214c1eba8fb4e04a68e932775
ljbmoura <ljbmoura@gmail.com>
2019-11-17 20:50:42 -0200

    aula3: Consumindo dados da API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({ // Decorator
   selector: 'app-root',
@@ -8,14 +9,15 @@ import { Component } from '@angular/core';
 export class AppComponent { // classe ECMAScript6
   title = 'alurapic';
 
-  photos = [
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Leão'
-    }
-    , {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg'
-      , description: 'Leoa'
-    }
-  ];
+  photos: Object[] = [];
+
+  constructor(http: HttpClient ) {
+    http
+      .get<Object[]>('http://localhost:3000/flavio/photos')
+      .subscribe( // Observable do RxJS
+         (dadosObservados) => { this.photos = dadosObservados; }
+         , (erroObservado) => { console.error(erroObservado.message); }
+      );
+    console.log(http);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    PhotosModule
+    BrowserModule
+    , PhotosModule
+    , HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
