34710f0105e31c1a9bf0c2db7de5777e9b445eae
ljbmoura <ljbmoura@gmail.com>
2019-11-17 22:36:18 -0200

    aula6: LoadButton

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -16,13 +17,18 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService) {
     console.log('construtor de PhotoListComponent executado.');
   }
 
   ngOnInit(): void {
 
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['fotos']; // ou .snapshot.data.photos;
     console.log(`this.photos recebeu ${this.photos.length} itens.`);
 
@@ -44,6 +50,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(
+        (itensProximaPagina) => {
+          this.photos = this.photos.concat(itensProximaPagina);
+          if(!itensProximaPagina.length) { this.hasMore = false; }
+        }
+      )
   }
 
 }
