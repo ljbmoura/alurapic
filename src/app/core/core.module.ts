83d0e04377508c0e7a23c49f8433e656f46fc719
ljbmoura <ljbmoura@gmail.com>
2019-11-18 20:52:27 -0200

    angular parte 2, aula 3: A diretiva routerLink

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho/cabecalho/cabecalho.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule
+    , RouterModule
   ]
   , declarations: [
     CabecalhoComponent
