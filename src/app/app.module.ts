4edcdaff239f50aa4fb12cf1082f037fd69aa782
ljbmoura <ljbmoura@gmail.com>
2019-11-17 22:10:41 -0200

    aula4: Lidando com rotas inexistentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule
     , AppRoutingModule
     , PhotosModule
+    , ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
