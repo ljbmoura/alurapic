51333443b1a994421278c4905469c8fa77ab7c8d
ljbmoura <ljbmoura@gmail.com>
2019-11-17 22:15:20 -0200

    aula4: Segmentos de rotas

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -11,12 +12,13 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
-
-  constructor(private photoService: PhotoService) {}
+  constructor(private photoService: PhotoService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    const userName = this.activatedRoute.snapshot.params.userName;
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe( // Observable do RxJS
         (dadosObservados) => { this.photos = dadosObservados; }
         , (erroObservado) => { console.error(erroObservado.message); }
