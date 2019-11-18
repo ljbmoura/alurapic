5ad1c317b2060727f0b9c972d7f4f3defc53871c
ljbmoura <ljbmoura@gmail.com>
2019-11-18 21:31:12 -0200

    angular parte 2, aula1: Enviando credenciais para a API

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const API_URL = 'http://localhost:3000';
+
+@Injectable ({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    constructor (private http: HttpClient) {}
+
+    authenticate (nomeUsuario: string, senha: string): Observable<Object> {
+        return this.http.post (
+            API_URL + '/user/login'
+            , { userName: nomeUsuario
+                , password: senha}
+        );
+    }
+}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { AuthService } from 'src/app/core/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
 @Component ({
     // Escopo de Página
     // selector: 'ap-signin', -> desnecessário pois nenhuma outra página usará este componente
@@ -10,13 +13,29 @@ export class SigninComponent implements OnInit {
 
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
 
     ngOnInit () {
         this.loginForm = this.formBuilder.group( {
-            userName: ['', Validators.required]
-            , password: ['', Validators.minLength(5)]
+            userName: ['flavio', Validators.required]
+            , password: ['123', Validators.minLength(3)]
         });
+    }
+
+    login () {
+        console.debug('submeteu');
+        this.authService.authenticate (
+            this.loginForm.get('userName').value
+            , this.loginForm.get('password').value)
+            .subscribe(
+                (usuarioAutenticado) => {
+                    console.log(`autenticação bem sucedida: ${JSON.stringify(usuarioAutenticado)}`);
+                }
+                , (erro: HttpErrorResponse) => {
+                    console.error(JSON.stringify(erro));
+                    alert(`Falha na autenticação: ${erro.error.message}`);
+                }
+            )
     }
 
 }
