3cdbca34607899d2c7d2f09886e95fbbdfc60091
ljbmoura <ljbmoura@gmail.com>
2019-11-18 20:39:19 -0200

    angular parte 2, aula 2: Acessando o headers

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -11,9 +12,20 @@ export class AuthService {
 
     constructor (private http: HttpClient) {}
 
-    authenticate (nomeUsuario: string, senha: string): Observable<Object> {
-        return this.http.post (API_URL + '/user/login'
-            , {userName: nomeUsuario, password: senha}
-        );
+    authenticate (nomeUsuario: string, senha: string): Observable<HttpResponse<Object>> {
+
+        return this.http
+            .post (
+                API_URL + '/user/login'
+                , { userName: nomeUsuario, password: senha}
+                , { observe: 'response', responseType: 'json'}
+            )
+            .pipe (
+                tap ( (response) => {
+                        const authToken = response.headers.get('x-access-token');
+                        // tslint:disable-next-line: no-console
+                        console.debug(`usuário ${nomeUsuario} autenticado com token ${authToken}`);
+                    })
+            );
     }
 }
