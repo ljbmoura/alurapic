161035c96af14a8804a09e588c185451e295f9f8
ljbmoura <ljbmoura@gmail.com>
2019-11-18 20:43:40 -0200

    angular parte 2, aula 2: Cabeçalho da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
-  imports: [
+  ]
+  , imports: [
     BrowserModule
     , AppRoutingModule
+    , CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
