c9e9e3ffbed2e70c64edbc4d641a37d8c3fcb0c1
ljbmoura <ljbmoura@gmail.com>
2019-11-18 21:24:25 -0200

    algular parte 2, aula1: Validação de formulários

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component ({
     // Escopo de Página
     // selector: 'ap-signin', -> desnecessário pois nenhuma outra página usará este componente
     templateUrl: './signin.component.html'
 })
-export class SigninComponent {}
+export class SigninComponent implements OnInit {
+
+    loginForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit () {
+        this.loginForm = this.formBuilder.group( {
+            userName: ['', Validators.required]
+            , password: ['', Validators.minLength(5)]
+        });
+    }
+
+}
