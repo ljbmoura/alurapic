687193ec2e13251b62e6636c39f78170f46fd897
ljbmoura <ljbmoura@gmail.com>
2019-11-20 19:07:02 -0200

    angular parte 3, aula 1: O formulário upload de fotos

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'ap-photo-form',
-  templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.css']
+  templateUrl: './photo-form.component.html'
 })
 export class PhotoFormComponent implements OnInit {
 
--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { PhotoFormComponent } from './photo-form.component';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [
@@ -9,6 +11,9 @@ import { PhotoFormComponent } from './photo-form.component';
     ],
     imports: [
         CommonModule
+        , VMessageModule
+        , ReactiveFormsModule
+        , FormsModule
     ]
 })
 export class PhotoFormModule {}
